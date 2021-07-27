2faf41f768bbcb1a9b9113ce11e1dd15379f8199
David <David>
2021-07-26 18:08:59 -0700

    test

--- run.py
+++ run.py
@@ -91,6 +91,7 @@ alice_examples = len(alice_data)
 bob_examples = len(bob_data)
 n_features = len(alice_data[0])
 n_epochs = settings_map['n_epochs']
+folds = settings_map['folds']
 
 file = []
 found_delim = False
@@ -111,6 +112,7 @@ file[start_of_delim + 1] = "alice_examples = {n}\n".format(n=alice_examples)
 file[start_of_delim + 2] = "bob_examples = {n}\n".format(n=bob_examples)
 file[start_of_delim + 3] = "n_features = {n}\n".format(n=n_features)
 file[start_of_delim + 4] = "n_epochs = {n}\n".format(n=n_epochs)
+file[start_of_delim + 5] = "folds = {n}\n".format(n=folds)
 
 # file as a string
 file = ''.join([s for s in file])
