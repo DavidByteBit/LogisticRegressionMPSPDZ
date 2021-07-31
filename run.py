39ea8b4ffe424cdcc7969ec1e58d73b8ae4452be
David <David>
2021-07-30 18:37:44 -0700

    test

--- run.py
+++ run.py
@@ -29,31 +29,31 @@ bob_data = []
 
 path = settings_map['alice_data_folder']
 
-x_train = path + "/test_X_fold{n}.csv".format(n=0)
-y_train = path + "/test_X_fold{n}.csv".format(n=0)
+x_train = path + "/train_X_fold{n}.csv".format(n=0)
+y_train = path + "/train_y_fold{n}.csv".format(n=0)
 x_test = path + "/test_X_fold{n}.csv".format(n=0)
-y_test = path + "/test_X_fold{n}.csv".format(n=0)
+y_test = path + "/test_y_fold{n}.csv".format(n=0)
 
 paths = [x_train, y_train, x_test, y_test]
 
 for p in paths:
     with open(p, 'r') as stream:
         for line in stream:
-            alice_data.append(line.split(","))
+            alice_data.extend(line.split(","))
 
 path = settings_map['bob_data_folder']
 
-x_train = path + "/test_X_fold{n}.csv".format(n=0)
-y_train = path + "/test_X_fold{n}.csv".format(n=0)
+x_train = path + "/train_X_fold{n}.csv".format(n=0)
+y_train = path + "/train_y_fold{n}.csv".format(n=0)
 x_test = path + "/test_X_fold{n}.csv".format(n=0)
-y_test = path + "/test_X_fold{n}.csv".format(n=0)
+y_test = path + "/test_y_fold{n}.csv".format(n=0)
 
 paths = [x_train, y_train, x_test, y_test]
 
 for p in paths:
     with open(p, 'r') as stream:
         for line in stream:
-            bob_data.append(line.split(","))
+            bob_data.extend(line.split(","))
 
 # 'command line arguments' for our .mpc file
 alice_examples = len(alice_data)
@@ -99,9 +99,9 @@ with open(settings_map['alice_private_input_path'], 'w') as stream:
 
     # Should just be one row I think, so I may clean this up a bit
     for row in alice_data:
-        s += " ".join(row) + " "
+        s += " ".join(row)
 
-    s = s[:-1]
+    stream.write(s)
 
 
 print("Alice has {n} many private values".format(n=len(alice_data)))
@@ -112,9 +112,7 @@ with open(settings_map['bob_private_input_path'], 'w') as stream:
 
     # Should just be one row I think, so I may clean this up a bit
     for row in bob_data:
-        s += " ".join(row) + " "
-
-    s = s[:-1]
+        s += " ".join(row)
 
     stream.write(s)
 
