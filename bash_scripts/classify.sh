92c1f14b96cb25ba95304b020ad4c0c0ce8a5c55
David <David>
2021-07-31 14:07:43 -0700

    test

--- bash_scripts/classify.sh
+++ bash_scripts/classify.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm ../spdz/Programs/Source/classifyLR.mpc
-cp runLR.mpc ../spdz/Programs/Source/classifyLR.mpc
+cp classifyLR.mpc ../spdz/Programs/Source/classifyLR.mpc
 ./../spdz/compile.py -R 64 -Z 2 classifyLR
