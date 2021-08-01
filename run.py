82708ac1a3c27c2556ff45f3b1252a2540068f33
David <David>
2021-08-01 12:25:55 -0700

    Clean up

--- run.py
+++ run.py
@@ -3,165 +3,187 @@ import subprocess
 import sys
 import math
 
-# Step 0,1: Setting everything up and re-writing the .mpc file
+
+# These files are apart of the repo and are assumed to remain in the relative path they were initially defined to be in
 mpc_file_path = "runLR.mpc"
 mpc_classify_file_path = "classifyLR.mpc"
-settings_map = None
 
-with open(sys.argv[1], 'r') as stream:
-    try:
-        settings_map = yaml.safe_load(stream)
-    except yaml.YAMLError as exc:
-        print(exc)
+# Entry point to run the script
+def run():
 
-folds = settings_map["folds"]
+    # Parse settings
+    settings_map = parse_settings()
 
-create_data = settings_map["create_data"]
+    # Grab settings
+    folds = settings_map["folds"]
 
-if create_data.lower() == "true":
-    subprocess.call(settings_map['path_to_this_repo'] + "/bash_scripts/create_data.sh %s" % folds, shell=True)
+    create_data = settings_map["create_data"]
 
-alice_data = []
-bob_data = []
+    # Determine if we need to create folds
+    if create_data.lower() == "true":
+        subprocess.call(settings_map['path_to_this_repo'] + "/bash_scripts/create_data.sh %s" % folds, shell=True)
 
-# 'command line arguments' for our .mpc file
-n_epochs = settings_map['n_epochs']
-folds = settings_map['folds']
-alice_examples = 0
-bob_examples = 0
-n_features = 0
-test_ratio = 0
+    # Upload Alice/Bob data to their respective private files
+    write_data(settings_map)
 
-path = settings_map['alice_data_folder']
+    # Edit .mpc source code for compilation
+    edit_source_code(settings_map)
 
-x_train = path + "/train_X_fold{n}.csv".format(n=0)
-y_train = path + "/train_y_fold{n}.csv".format(n=0)
-x_test = path + "/test_X_fold{n}.csv".format(n=0)
-y_test = path + "/test_y_fold{n}.csv".format(n=0)
+    # Compile .mpc program
+    subprocess.call(settings_map['path_to_this_repo'] + "/bash_scripts/compile.sh")
+    # subprocess.call(settings_map['path_to_this_repo'] + "/bash_scripts/classify.sh")
 
-paths = [x_train, y_train, x_test, y_test]
 
-feature_length_not_found = True
+def edit_source_code(settings_map):
 
-for p in paths:
-    with open(p, 'r') as stream:
-        row = 0
+    # 'command line arguments' for our .mpc file
+    n_epochs = settings_map['n_epochs']
+    folds = settings_map['folds']
+    alice_examples = 0
+    bob_examples = 0
+    n_features = 0
+    # test_ratio = 0
+
+    file = []
+    found_delim = False
+    start_of_delim = 0
+
+    i = 0
+    with open(mpc_file_path, 'r') as stream:
         for line in stream:
-            if feature_length_not_found and "train_X_fold" in p:
-                feature_length_not_found = False
-                n_features = len(line.replace("\n", "").split(","))
-            row += 1
-            alice_data.extend(line.replace("\n", "").split(","))
 
-        if "train_X_fold" in p:
-            alice_examples = row
+            if not found_delim and "@args" in line:
+                start_of_delim = i
+                found_delim = True
+            i += 1
 
+            file.append(line)
 
-path = settings_map['bob_data_folder']
+    file[start_of_delim + 1] = "alice_examples = {n}\n".format(n=alice_examples)
+    file[start_of_delim + 2] = "bob_examples = {n}\n".format(n=bob_examples)
+    file[start_of_delim + 3] = "n_features = {n}\n".format(n=n_features)
+    file[start_of_delim + 4] = "n_epochs = {n}\n".format(n=n_epochs)
+    file[start_of_delim + 5] = "folds = {n}\n".format(n=folds)
 
-x_train = path + "/train_X_fold{n}.csv".format(n=0)
-y_train = path + "/train_y_fold{n}.csv".format(n=0)
-x_test = path + "/test_X_fold{n}.csv".format(n=0)
-y_test = path + "/test_y_fold{n}.csv".format(n=0)
+    # file as a string
+    file = ''.join([s for s in file])
 
-paths = [x_train, y_train, x_test, y_test]
+    # print(file)
 
-for p in paths:
-    with open(p, 'r') as stream:
-        row = 0
+    with open(mpc_file_path, 'w') as stream:
+        stream.write(file)
+
+    ################### Next one ######################
+
+    file = []
+    found_delim = False
+    start_of_delim = 0
+
+    i = 0
+    with open(mpc_classify_file_path, 'r') as stream:
         for line in stream:
-            row += 1
-            bob_data.extend(line.replace("\n", "").split(","))
 
-        if "train_X_fold" in p:
-            bob_examples = row
+            if not found_delim and "@args" in line:
+                start_of_delim = i
+                found_delim = True
+            i += 1
 
+            file.append(line)
 
-file = []
-found_delim = False
-start_of_delim = 0
+    test_ratio = 1 / folds
+    alice_test_examples = int(math.ceil(alice_examples * test_ratio))
+    bob_test_examples = int(bob_examples * test_ratio)
 
-i = 0
-with open(mpc_file_path, 'r') as stream:
-    for line in stream:
+    file[start_of_delim + 1] = "alice_examples = {n}\n".format(n=alice_test_examples)
+    file[start_of_delim + 2] = "bob_examples = {n}\n".format(n=bob_test_examples)
+    file[start_of_delim + 3] = "n_features = {n}\n".format(n=n_features)
 
-        if not found_delim and "@args" in line:
-            start_of_delim = i
-            found_delim = True
-        i += 1
+    # file as a string
+    file = ''.join([s for s in file])
 
-        file.append(line)
+    # print(file)
 
-file[start_of_delim + 1] = "alice_examples = {n}\n".format(n=alice_examples)
-file[start_of_delim + 2] = "bob_examples = {n}\n".format(n=bob_examples)
-file[start_of_delim + 3] = "n_features = {n}\n".format(n=n_features)
-file[start_of_delim + 4] = "n_epochs = {n}\n".format(n=n_epochs)
-file[start_of_delim + 5] = "folds = {n}\n".format(n=folds)
-
-# file as a string
-file = ''.join([s for s in file])
-
-# print(file)
-
-with open(mpc_file_path, 'w') as stream:
-    stream.write(file)
-
-################### Next one ######################
-
-file = []
-found_delim = False
-start_of_delim = 0
-
-i = 0
-with open(mpc_classify_file_path, 'r') as stream:
-    for line in stream:
-
-        if not found_delim and "@args" in line:
-            start_of_delim = i
-            found_delim = True
-        i += 1
-
-        file.append(line)
-
-test_ratio = 1 / folds
-alice_test_examples = int(math.ceil(alice_examples * test_ratio))
-bob_test_examples = int(bob_examples * test_ratio)
-
-file[start_of_delim + 1] = "alice_examples = {n}\n".format(n=alice_test_examples)
-file[start_of_delim + 2] = "bob_examples = {n}\n".format(n=bob_test_examples)
-file[start_of_delim + 3] = "n_features = {n}\n".format(n=n_features)
-
-# file as a string
-file = ''.join([s for s in file])
-
-# print(file)
-
-with open(mpc_classify_file_path, 'w') as stream:
-    stream.write(file)
+    with open(mpc_classify_file_path, 'w') as stream:
+        stream.write(file)
 
 
-# Step 2: write to Alice's and Bobs private input files
-with open(settings_map['alice_private_input_path'], 'w') as stream:
+def write_data(settings_map):
+    path = settings_map['alice_data_folder']
 
-    s = " ".join(alice_data)
+    x_train = path + "/train_X_fold{n}.csv".format(n=0)
+    y_train = path + "/train_y_fold{n}.csv".format(n=0)
+    x_test = path + "/test_X_fold{n}.csv".format(n=0)
+    y_test = path + "/test_y_fold{n}.csv".format(n=0)
 
-    stream.write(s)
+    paths = [x_train, y_train, x_test, y_test]
+
+    feature_length_not_found = True
+
+    alice_data = []
+    bob_data = []
+
+    for p in paths:
+        with open(p, 'r') as stream:
+            row = 0
+            for line in stream:
+                if feature_length_not_found and "train_X_fold" in p:
+                    feature_length_not_found = False
+                    n_features = len(line.replace("\n", "").split(","))
+                row += 1
+                alice_data.extend(line.replace("\n", "").split(","))
+
+            if "train_X_fold" in p:
+                alice_examples = row
+
+    path = settings_map['bob_data_folder']
+
+    x_train = path + "/train_X_fold{n}.csv".format(n=0)
+    y_train = path + "/train_y_fold{n}.csv".format(n=0)
+    x_test = path + "/test_X_fold{n}.csv".format(n=0)
+    y_test = path + "/test_y_fold{n}.csv".format(n=0)
+
+    paths = [x_train, y_train, x_test, y_test]
+
+    for p in paths:
+        with open(p, 'r') as stream:
+            row = 0
+            for line in stream:
+                row += 1
+                bob_data.extend(line.replace("\n", "").split(","))
+
+            if "train_X_fold" in p:
+                bob_examples = row
+
+    # Step 2: write to Alice's and Bobs private input files
+    with open(settings_map['alice_private_input_path'], 'w') as stream:
+
+        s = " ".join(alice_data)
+
+        stream.write(s)
+
+    print("Alice has {n} many private values".format(n=len(alice_data)))
+
+    with open(settings_map['bob_private_input_path'], 'w') as stream:
+
+        s = " ".join(bob_data)
+
+        stream.write(s)
+
+    print("Bob has {n} many private values".format(n=len(bob_data)))
+
+def parse_settings():
+    settings_map = None
+
+    with open(sys.argv[1], 'r') as stream:
+        try:
+            settings_map = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    return settings_map
 
 
-print("Alice has {n} many private values".format(n=len(alice_data)))
-
-with open(settings_map['bob_private_input_path'], 'w') as stream:
-
-    s = " ".join(bob_data)
-
-    stream.write(s)
-
-
-print("Bob has {n} many private values".format(n=len(bob_data)))
-
-# Step 3: Compile .mpc program
-subprocess.call(settings_map['path_to_this_repo'] + "/bash_scripts/compile.sh")
-#subprocess.call(settings_map['path_to_this_repo'] + "/bash_scripts/classify.sh")
+run()
 
 input("Press enter to populate Alice's and Bobs data with the next fold")
 
