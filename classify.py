f61360ed5f0101eddb6725b6c6bf48b4233fdd47
David <David>
2021-08-02 12:01:30 -0700

    test

--- classify.py
+++ classify.py
@@ -1,5 +1,19 @@
 
 
+def sig(x):
+    return 1 / (1 + 1 / (2.71 ** x))
+
+def dp(a, b):
+
+    assert (len(a) == len(b))
+
+    res = 0
+
+    for i in range(len(a)):
+        res += a[i] * b[i]
+
+    return res
+
 def parse_file(file_path):
 
     output = []
@@ -14,6 +28,30 @@ def parse_file(file_path):
                 delim = i
             i += 1
 
-    bias = output[delim + 2]
+    bias = output[delim + 2].replace("\n", "")
     weights_mid = output[delim + 3]
 
+    weights_mid = weights_mid.replace("[", "").replace("]", "").replace("\n", "")
+
+    weights = weights_mid.split(",")
+
+    return bias, weights
+
+
+def load_test_data(directory, fold):
+
+    feature_path = directory + "test_X_fold{n}.csv".format(n=fold)
+    label_path = directory + "test_y_fold{n}.csv".format(n=fold)
+
+    data = []
+    label = []
+
+    with open(feature_path, 'r') as f:
+        for line in f:
+            data.append(line.replace("\n", "").split(","))
+
+    with open(label_path, 'r') as f:
+        for line in f:
+            label.append(line.replace("\n", ""))
+
+    return data, label
