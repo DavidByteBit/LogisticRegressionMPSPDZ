b0cf33d954b0152cf08ca0241361df8d4f8cd975
David <David>
2021-08-02 16:42:34 -0700

    test

--- create_data.py
+++ create_data.py
@@ -6,9 +6,6 @@
 
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 from sklearn.model_selection import KFold
 from sklearn import preprocessing
 from numpy import savetxt
