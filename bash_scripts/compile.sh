2a82d2324df9a1cd161f1c02bc54fe90fca6af98
David <David>
2021-08-08 10:46:36 -0700

    test

--- bash_scripts/compile.sh
+++ bash_scripts/compile.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+rm ../spdz/Programs/Source/lr.py
+cp lr.py ../spdz/Programs/Source/lr.py
+
 rm ../spdz/Programs/Source/runLR.mpc
 cp runLR.mpc ../spdz/Programs/Source/runLR.mpc
 ./../spdz/compile.py -R 64 -Z 2 runLR
