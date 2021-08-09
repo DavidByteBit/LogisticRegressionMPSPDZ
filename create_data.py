8589fb9d0b6765987847f67622df509be8da3b71
David <David>
2021-08-09 11:41:09 -0700

    test

--- create_data.py
+++ create_data.py
@@ -14,7 +14,6 @@ import sys
 num_of_folds = int(sys.argv[1])
 
 
-
 ##################################################################################
 
 def preprocess(dirty_df):
@@ -22,11 +21,11 @@ def preprocess(dirty_df):
     target_map = {u'1': 1, u'0': 0}
     dirty_df['__target__'] = dirty_df['cohort_flag'].map(str).map(target_map)
     dirty_df = dirty_df.drop(['cohort_flag'], axis=1)
-    clean_X = dirty_df.drop('__target__', axis=1)
+    clean_X = dirty_df.drop('__target__', axis=1).iloc[:, :100]
     clean_X = clean_X.to_numpy()
     clean_X = preprocessing.normalize(clean_X, norm='l2')
     clean_y = np.array(dirty_df['__target__'])
-    return clean_X, clean_y
+    return clean_X[:200], clean_y[:200]
 
 
 ##################################################################################
@@ -57,7 +56,6 @@ mylambda = 0.5
 fold = 0
 
 for (train1_indices, test1_indices), (train2_indices, test2_indices) in zip(kf1.split(X1, y1), kf2.split(X2, y2)):
-
     print("Starting to process fold {n}".format(n=fold))
 
     AliceX_train, AliceX_test = X1[train1_indices, :].tolist(), X1[test1_indices, :].tolist()
