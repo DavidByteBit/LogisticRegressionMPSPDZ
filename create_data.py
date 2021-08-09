8fe762c400c753c60bf83fd184c0004861b97085
David <David>
2021-08-09 15:35:26 -0700

    test

--- create_data.py
+++ create_data.py
@@ -21,11 +21,11 @@ def preprocess(dirty_df):
     target_map = {u'1': 1, u'0': 0}
     dirty_df['__target__'] = dirty_df['cohort_flag'].map(str).map(target_map)
     dirty_df = dirty_df.drop(['cohort_flag'], axis=1)
-    clean_X = dirty_df.drop('__target__', axis=1)
+    clean_X = dirty_df.drop('__target__', axis=1).iloc[: , : 100]
     clean_X = clean_X.to_numpy()
     clean_X = preprocessing.normalize(clean_X, norm='l2')
     clean_y = np.array(dirty_df['__target__'])
-    return clean_X, clean_y
+    return clean_X[:200], clean_y[:200]
 
 
 ##################################################################################
