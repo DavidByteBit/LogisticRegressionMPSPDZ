b95d556b77b9577709e416a049a16272a60b7335
David <David>
2021-08-16 12:33:23 -0700

    t

--- bash_scripts/compile.sh
+++ bash_scripts/compile.sh
@@ -4,6 +4,6 @@ compile=$1
 
 rm ../spdz/Programs/Source/runLR.mpc
 cp runLR.mpc ../spdz/Programs/Source/runLR.mpc
-cp generate_noise.py ../spdz/Programs/Source/generate_noise.py
+cp generate_noise.py ../spdz/Compiler/generate_noise.py
 echo running ./../spdz/compile.py "$compile"
 ./../spdz/compile.py $compile
