831de85ce20969140c0931ed3ed3efe84f9dd6d5
Ricardo José Menezes Maia <ricardo.jmm@gmail.com>
2021-08-18 01:22:21 -0300

    scripts for executing any parties

--- run_party0.sh
+++ run_party0.sh
@@ -3,8 +3,9 @@ party=0
 docker run -p 1728:1728 -p 1729:1729 -e port=1729 -e party=0 \
    -e ip_source="192.168.0.13" -e protocol="replicated-ring-party.x"  \
    -e data_path="/opt/app/MP-SPDZ/data/IDASH-ALICE/party_2.csv" \
-   -e save_folder="save_folder" -e process_labels="true" \
+   -e save_folder="/opt/app/MP-SPDZ/results" -e process_labels="true" \
    -e lambda=1 -e n_epochs=10 -e batch_size=128 -e n_folds=5 \
-   -v $pwd/PPMLRobots/:/opt/app/MP-SPDZ/data/
-   -v $pwd/logs/:/opt/app/MP-SPDZ/logs/
+   -v $(pwd)/logs:/opt/app/MP-SPDZ/logs \
+   -v $(pwd)/results:/opt/app/MP-SPDZ/results \
+   -v $(pwd)/PPMLRobots:/opt/app/MP-SPDZ/data \
    ricardojmmaia/idash2021 sh /opt/app/MP-SPDZ/run_all_steps.sh 
--- run_party2.sh
+++ run_party2.sh
@@ -1,5 +1,11 @@
 
-docker run -p 1731:1731 -e port=1729 -e party=0 -e ip_source="192.168.0.13" \
--e data_path="data_path" -e save_folder="save_folder" -e process_labels="true" \
--e protocol="replicated-ring-party.x" -e lambda=1 -e n_epochs=10 -e batch_size=128 \
-ricardojmmaia/idash2021 sh /opt/app/MP-SPDZ/run_all_steps.sh  &
+
+docker run -p 1731:1731 -e port=1729 -e party=2 -e ip_source="192.168.0.13" \
+   -e data_path="data_path" -e save_folder="save_folder" \
+   -e process_labels="true" \
+   -e protocol="replicated-ring-party.x" -e lambda=1 -e n_epochs=10 \
+   -e batch_size=128 \
+   -v $(pwd)/logs:/opt/app/MP-SPDZ/logs \
+   -v $(pwd)/results:/opt/app/MP-SPDZ/results \
+   -v $(pwd)/PPMLRobots:/opt/app/MP-SPDZ/data \
+   ricardojmmaia/idash2021 sh /opt/app/MP-SPDZ/run_all_steps.sh  &
