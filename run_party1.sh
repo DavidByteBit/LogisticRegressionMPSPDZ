ef628aeb8d68a43a14f66c7eb93862d35e355992
Ricardo José Menezes Maia <ricardo.jmm@gmail.com>
2021-08-18 12:54:20 -0300

    scripts for tests using replicated MPC

--- run_party1.sh
+++ run_party1.sh
@@ -1,4 +1,4 @@
-
+ 
 docker run -p 1730:1730 -e port=1729 -e party=1 -e ip_source="192.168.0.13" \
    -e protocol="replicated-ring-party.x"  \
    -e process_labels="true" \
@@ -6,9 +6,10 @@ docker run -p 1730:1730 -e port=1729 -e party=1 -e ip_source="192.168.0.13" \
    -e save_folder="/opt/app/MP-SPDZ/save/" \
    -e path_to_model="/opt/app/MP-SPDZ/save/"  \
    -e test_data_folder="/opt/app/MP-SPDZ/save/" \
-   -e prediction_file_path="/opt/app/MP-SPDZ/save/" \
+   -e prediction_file_path="/opt/app/MP-SPDZ/save/predictions" \
    -e epsilon=1 -e lambda=1 -e num_epochs=300 -e batch_size=128 \
    -e N1=832 -e N2=832 -e num_features=1784 \
    -v $(pwd)/save:/opt/app/MP-SPDZ/save \
    -v $(pwd)/PPMLRobots:/opt/app/MP-SPDZ/data \
    ricardojmmaia/idash2021 sh /opt/app/MP-SPDZ/run_all_steps.sh 
+
--- run_party2.sh
+++ run_party2.sh
@@ -1,12 +1,12 @@
-
+ 
 docker run -p 1731:1731 -e port=1729 -e party=2 -e ip_source="192.168.0.13" \
    -e protocol="replicated-ring-party.x"  \
    -e process_labels="true" \
-   -e data_path="/opt/app/MP-SPDZ/data/party_2.csv" \
+   -e data_path="/opt/app/MP-SPDZ/data/party_1.csv" \
    -e save_folder="/opt/app/MP-SPDZ/save/" \
    -e path_to_model="/opt/app/MP-SPDZ/save/"  \
    -e test_data_folder="/opt/app/MP-SPDZ/save/" \
-   -e prediction_file_path="/opt/app/MP-SPDZ/save/" \
+   -e prediction_file_path="/opt/app/MP-SPDZ/save/predictions" \
    -e epsilon=1 -e lambda=1 -e num_epochs=300 -e batch_size=128 \
    -e N1=832 -e N2=832 -e num_features=1784 \
    -v $(pwd)/save:/opt/app/MP-SPDZ/save \
