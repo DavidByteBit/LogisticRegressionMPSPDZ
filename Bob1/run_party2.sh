1a19f73ff743c0c7ae373c1e24ea72a1dcc4c3a5
Ricardo José Menezes Maia <ricardo.jmm@gmail.com>
2021-08-18 20:17:40 -0300

    included keys and pems and folders for test

---
+++ Bob1/run_party2.sh
@@ -0,0 +1,17 @@
+ 
+docker run -p 1731:1731 -e port=1729 -e party=2 -e ip_source="192.168.0.13" \
+   -e protocol="replicated-ring-party.x"  \
+   -e process_labels="true" \
+   -e data_path="/opt/app/MP-SPDZ/data/party_0.csv" \
+   -e save_folder="/opt/app/MP-SPDZ/model/" \
+   -e path_to_model="/opt/app/MP-SPDZ/model/weigths.txt"  \
+   -e test_data_folder="/opt/app/MP-SPDZ/test/" \
+   -e prediction_file_path="/opt/app/MP-SPDZ/test/predictions.txt" \
+   -e epsilon=1 -e lambda=1 -e num_epochs=300 -e batch_size=128 \
+   -e N1=882 -e N2=831 -e num_features=1874 \
+   -e task_training_testing="training" \
+   -v $(pwd)/save:/opt/app/MP-SPDZ/save \
+   -v $(pwd)/data:/opt/app/MP-SPDZ/data \
+   -v $(pwd)/Player-Data:/opt/app/MP-SPDZ/Player-Data \
+   -v $(pwd)/test:/opt/app/MP-SPDZ/test \
+   ricardojmmaia/idash2021 
